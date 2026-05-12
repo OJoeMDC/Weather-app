a49a410e6b69f395080e36ad0b1702f8921cc018
OJoeMDC <ojoe.mdc@gmail.com>
2026-05-11 21:17:28 -0400

    Added more display options

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,46 @@ import WeatherScroller from './WeatherScroller'
 // key="50d464538c244551b7d230839261005";
 
 
+
 function App() {
+const [error, setError] = useState("");
+const [selectedCity, setSelectedCity] = useState("London");
+
   const [tempF, setTempF] = useState(null);
   const [weather, setWeather] = useState(null);
 
   async function getWeather () {
-    const response = await fetch(
-    "https://api.weatherapi.com/v1/forecast.json?key=50d464538c244551b7d230839261005&q=London&days=1&aqi=no&alerts=no"
-      );
-    const weatherData = await response.json();
+    try {
+      const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=50d464538c244551b7d230839261005&q=${selectedCity}&days=1&aqi=no&alerts=n`
+        );
+      const weatherData = await response.json();
 
-    const tempC = weatherData.current?.temp_c;
-    const tempFar = weatherData.current?.temp_f;
+      const tempC = weatherData.current?.temp_c;
+      const tempFar = weatherData.current?.temp_f;
 
-    setTempF(tempFar);
-    setWeather(weatherData);
+      setTempF(tempFar);
+      setWeather(weatherData);
 
-    console.log(weatherData);
+      console.log(weatherData);
+
+
+    } catch (err) {
+
+      setError(err.message);
+    }
   }
 
+
   useEffect(() => {
-    getWeather();
-  }, []);
+      getWeather();
+      }, []);
+    
 
   return (
     <>
     <main>
-      {weather && <WeatherScroller weather={weather} />}
+      {weather && <WeatherScroller weather={weather} selectedCity={selectedCity} setSelectedCity={setSelectedCity}/>}
     </main>
     </>
   )
--- src/weatherCard.jsx
+++ src/weatherCard.jsx
@@ -3,19 +3,51 @@ import './WeatherCard.css'
 
 
 function WeatherCard({weather}) {
+
+const [hour, setHour] = useState("0");
+const [day, setDay] = useState("0");
+
+const selectedDay = weather?.forecast?.forecastday?.[Number(day)];
+const selectedHour = selectedDay?.hour?.[Number(hour)];
 const city = weather.location.name;
+
+// if (!selectedDay || !selectedHour) {
+//     return null;
+// }
 const tempF = weather.current.temp_f;
-const maxTemp = weather.forecast.forecastday[0].day.maxtemp_f;
-const minTemp = weather.forecast.forecastday[0].day.mintemp_f;
+
 
     return (
         <div className='weather-card'>
             {/* <h2>title</h2> */}
             <h3>City: {city}</h3>
-            <p>Now: {tempF}</p>
-            <p>High: {maxTemp}</p>
-            <p>Low: {minTemp}</p>
+            {/* <p>Now: {tempF}</p>
+            <p>Hour 0: {hourF}</p> */}
+
+            {selectedDay? (
+                <>
+                <p>High: {selectedDay.day.maxtemp_f}</p>
+                <p>Low: {selectedDay.day.mintemp_f}</p>
+                </>
+            ) : (
+                <p>Invalid Day</p>
+            )}
+
+            {selectedHour? (
+                <>
+                <p>Hour {hour ? hour: 0}: {selectedHour.temp_f}</p>
+                </>
+            ) : (
+                <p>Invalid Hour</p>
+            )}
+
+            <label>Day</label>
+            <input className='Day' type='number' value={day} onChange={(e) => setDay(e.target.value)} />
+            <label>Hour</label>
+            <input className='Hour' type='number' value={hour} onChange={(e) => setHour(e.target.value)} />
+
         </div>
+
     );
 }
 
